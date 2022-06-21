b1d55c56258f5ef92ab998febd4657ccc65d1cd1
JoseAP0 <j.augustoifg@gmail.com>
2022-06-21 01:20:49 -0300

    Booking form almost ready

--- app/controllers/bookings_controller.rb
+++ app/controllers/bookings_controller.rb
@@ -1,35 +1,29 @@
 class BookingsController < ActionController::Base
 
-  def index
-    @booking = Booking.all
-  end
-
-  def show
-  end
-
   def new
     @booking = Booking.new
   end
 
   def create
 
-    @booking = Booking.new(Booking_params)
+    @booking = Booking.new(bookings_params)
 
     respond_to do |format|
       if @booking.save
-        format.html { redirect_to booking_url(@booking), notice: "Test was successfully created." }
-        format.json { render :show, status: :created, location: @booking }
+        format.html { redirect_to new_booking_path, notice: "Your booking was completed." }
+        # format.json { render :show, status: :created, location: @booking }
       else
         format.html { render :new, status: :unprocessable_entity }
-        format.json { render json: @booking.errors, status: :unprocessable_entity }
+        # format.json { render json: @booking.errors, status: :unprocessable_entity }
       end
     end
   end
 
   private
 
-  def Booking_params
-    params.fetch(:Booking, {})
+  def bookings_params
+    params.require(:booking).permit(:full_name, :phone, :email, :cpf, :annotation, :booking_date, :booking_time_start, :booking_time_ending)
   end
+
 end
 
