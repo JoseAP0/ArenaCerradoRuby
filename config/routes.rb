48ed8deaa2433c4601b2679e22bc3822819dc5e2
JoseAP0 <j.augustoifg@gmail.com>
2022-07-06 12:15:48 -0300

    intial login added

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,11 @@
 Rails.application.routes.draw do
+  devise_for :views
+  devise_for :users
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   resources :login
 
   resources :bookings
-
   # Defines the root path route ("/")
   root "home#index"
 end
